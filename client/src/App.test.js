a6cb535350e3e627da3e555107bcc229fa9d15f1
ofir <zcharya@post.bgu.ac.il>
2025-07-09 15:41:19 +0300

    Dismissed App.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,13 +1,3 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-test("renders learn react link", async () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  const linkElement = await screen.findByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
